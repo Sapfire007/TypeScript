0cc0d76d2a577c9ede37b5c5a386f5e2ccb47070
Sapfire007 <sapfire955@gmail.com>
2025-12-05 20:36:21 +0530

    Added Array Types and Object Types

---
+++ 3. Array Types/Multidimensional-Arrays.ts
@@ -0,0 +1,7 @@
+const singleDi: number[] = [1, 2, 3, 4, 5];
+const doubleDi: number[][] = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]];
+const tripleDi: number[][][] = [[[1, 2, 3, 4, 5]]];
+
+console.log(singleDi);
+console.log(doubleDi);
+console.log(tripleDi);
